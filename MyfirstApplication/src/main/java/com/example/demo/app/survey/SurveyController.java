0407430749efff7c7ca823e109dc60e1ff58ff8d
furutanisann <kyoto.furutani@gmail.com>
2022-02-04 08:23:40 +0900

    簡単なお問い合わせフォームまで（機能のみ）

--- MyfirstApplication/src/main/java/com/example/demo/app/survey/SurveyController.java
+++ MyfirstApplication/src/main/java/com/example/demo/app/survey/SurveyController.java
@@ -7,10 +7,13 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.app.inquiry.InquiryForm;
 
-@Controller("/survey")
+@Controller
+@RequestMapping("/survey")
 public class SurveyController {
 //  トップページ（入力用）
 	@GetMapping("/form")
@@ -24,7 +27,7 @@ public class SurveyController {
 
 //	確認画面に移動
 	@PostMapping("/confirm")
-	public String confirm(@Validated InquiryForm inquiryForm,
+	public String confirm(@Validated SurveyForm surveyForm,
 			              Model model,
 //					   バリデーション結果を受け取っている
 			              BindingResult result) {
@@ -37,6 +40,29 @@ public class SurveyController {
 		model.addAttribute("title","確認ページ");
 		return "survey/confirm";
 	}
+
+//  再編集でもどる
+	@PostMapping("/form")
+	public String formBack(SurveyForm survayForm,
+						   Model model) {
+		model.addAttribute("title","メインページに戻りました");
+		return "survey/form";
+	}
+	
+//  送信を押して終了後の処理
+	@PostMapping("/complete")
+	public String complete(@Validated SurveyForm survayForm,
+						   BindingResult result,
+						   Model model,
+						   RedirectAttributes redirecutAttributes ) {
+		if(result.hasErrors()) {
+			model.addAttribute("title","メインページに戻りました");
+			return "survey/form";
+		}
+		redirecutAttributes.addAttribute("complete","Registered");
+		return "redirect:/survey/form";
+	}
+	   
 	
 	          
 }
